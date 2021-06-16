4d4aca0a684cfa60c327a7b76c01418609723406
ruagegd <elena.kosheleva@raiffeisen.ru>
2021-06-16 17:38:38 +0300

    v2

--- src/main/java/net/server/Server.java
+++ src/main/java/net/server/Server.java
@@ -11,43 +11,47 @@ public class Server {
 
     public static void main(String[] args) {
         System.out.println("server started");
+        try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT)) {
+            while (true) {
 
-        while (true) {
+                try (Socket socket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                ) {
+                    System.out.println("New connection accepted");
+                    out.println("Write your name:");
+                    final String name = in.readLine();
 
-            try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
-                 Socket socket = serverSocket.accept();
-                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            ) {
-                System.out.println("New connection accepted");
-                out.println("Write your name:");
-                final String name = in.readLine();
-                out.println("Are you child? (yes/no)");
-                if (in.readLine().equals("yes")) {
-                    out.println(String.format("Welcome to the kids area, %s Let's play! (Press \"Enter\" to continue)"
-                            , name));
+                    out.println("Are you child? (yes/no)");
+                    if (in.readLine().equals("yes")) {
+                        out.println(String.format("Welcome to the kids area, %s Let's play! (Press \"Enter\" to continue)"
+                                , name));
+                        in.readLine();
+                        out.println("What game do you want to play?");
+                        final String game = in.readLine();
+                        out.println(String.format("%s is a great game! (Press \"Enter\" to finish)", game));
+                    } else {
+                        out.println(String.format("Welcome to the adult zone, %s Have a good rest, or a good working day!" +
+                                "(Press \"Enter\" to continue)", name));
+                        in.readLine();
+                        out.println("How do you prefer to spend your free time?");
+                        final String freeTime = in.readLine();
+                        out.println(String.format("I also like to %s (Press \"Enter\" to finish)", freeTime));
+                    }
                     in.readLine();
-                    out.println("What game do you want to play?");
-                    final String game = in.readLine();
-                    out.println(String.format("%s is a great game! (Press \"Enter\" to finish)", game));
-                } else {
-                    out.println(String.format("Welcome to the adult zone, %s Have a good rest, or a good working day!" +
-                            "(Press \"Enter\" to continue)", name));
-                    in.readLine();
-                    out.println("How do you prefer to spend your free time?");
-                    final String freeTime = in.readLine();
-                    out.println(String.format("I also like to %s (Press \"Enter\" to finish)", freeTime));
+                    out.println("end");
+                    System.out.println("session ends");
+
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-                in.readLine();
-                out.println("end");
-                System.out.println("session ends");
+                ;
 
-            } catch (Exception e) {
-                e.printStackTrace();
             }
-            ;
-
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        ;
 
     }
 }
